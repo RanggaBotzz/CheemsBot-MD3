4e5f293198ef924b7c0ca413e87f5f1f2cf3e5cf
RANGGA <107257184+RanggaBotzz@users.noreply.github.com>
2022-07-05 11:51:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,14 +41,14 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc"
-global.ytname = "YT:ARULL OFC " //ur yt chanel name
+global.owner = ['6285779150501'] //ur owner number
+global.ownername = "HanzOfc"
+global.ytname = "YT: Hanz OFC " //ur yt chanel name
 global.socialm = "GitHub: Warikrr" //ur github or insta name
 global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
 
 //bot bombdy
-global.botname = "ArullBotz" //ur bot name
+global.botname = "HanzBotz" //ur bot name
 global.websitex = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
 global.packname = "Sticker By"
@@ -56,8 +56,8 @@ global.author = "ArullBotz"
 global.themeemoji = "🐶"
 global.reactmoji = "🗿"
 global.ownertag = ['ArullOfc'] //ur owner tag
-global.ownernummenu = ['6285822347348'] //ur owner number in menu and all
-global.watermark = "ArullBotz" //ur watermark
+global.ownernummenu = ['6285779150501'] //ur owner number in menu and all
+global.watermark = "HanzBotz" //ur watermark
 global.botscript = "https://youtu.be/Gr6yuCbSGTI"
 global.linkz1 = "https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKAS"
 global.linkz2 = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg"
